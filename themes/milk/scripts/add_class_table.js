ab07a09c0ff87130973f424f783eceebc265b322
xcatliu <xcatliu@gmail.com>
2018-09-20 13:13:21 +0800

    Update replace

--- themes/milk/scripts/add_class_table.js
+++ themes/milk/scripts/add_class_table.js
@@ -1,6 +1,6 @@
 /* global hexo */
 
 hexo.extend.filter.register('after_post_render', (data) => {
-  data.content = data.content.replace(/<table>/, '<table class="table">');
+  data.content = data.content.replace(/<table>/g, '<table class="table">');
   return data;
 });
